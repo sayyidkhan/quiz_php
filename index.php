0a6636629f5c7ef77e41ab56cfa085fb61c12472
User <user@Users-MacBook-Air.local>
2021-07-25 12:42:03 +0800

    add homepage

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
+  session_start();
+
   define('CSS_PATH', 'css/'); //define bootstrap css path
   define('IMG_PATH','./img/'); //define img path
   $main_css = 'main.css'; // main css filename
+  $flex_css = 'flex.css'; // flex css filename
+  $tableui_css = 'tableui.css'; // flex css filename
 ?>
 
 <!DOCTYPE html>
@@ -9,9 +13,11 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Takoko - Bike Shop</title>
+    <title>Takoko Quiz- Score It</title>
     <!-- main CSS-->
     <link rel="stylesheet" href='<?php echo (CSS_PATH . "$main_css"); ?>' type="text/css">
+    <link rel="stylesheet" href='<?php echo (CSS_PATH . "$flex_css"); ?>' type="text/css">
+    <link rel="stylesheet" href='<?php echo (CSS_PATH . "$tableui_css"); ?>' type="text/css">
 
   </head>
   <body>
@@ -19,38 +25,112 @@
       <div id="wrapper">
 
         <div id="header">
-          <h1>Takoko</h1>
-          <div id="nav">
-            <ul>
-              <li><a href="index.php">Home</a></li>
-              <li><a href="addbikelisting.php">Add Listing</a></li>
-              <li><a href="managelisting.php">Manage Listing</a></li>
-              <li><a href="bikelisting.php">View Listing's</a></li>
-            </ul>
-          </div>
+          <h1>Takoko Quiz</h1>
         </div>
 
         <div id="content">
 
-           <div>
-            <h3 class="centerText primarycolor">ONLINE MARKETPLACE TO BUY & SELL BIKES</h3>
+           <section id='login-section'>
+            <h3 class="centerText primarycolor">Welcome to Quiz Center. Enter your login details to proceed</h3>
 
             <div class="container">
-              <img 
-              src="<?php echo (IMG_PATH . 'mainpage-bicycle.jpg') ?>"
-              alt="online bike marketplace"
-              width="500"
-              height="400"
-              loading="lazy">
-            </div>
+              <form 
+              method='post'
+              name='login-name'
+              class="bikesearch boxsizing"
+              action="#quizselection-section" 
+              style="margin:auto;max-width:600px;margin-bottom: 2em;padding-left: 4em;padding-right: 2em;">
+                <?php
+                $login = $_SESSION["login"];
+                $disabled = empty($_SESSION['login']) ? '' : 'disabled="true"';
+                $bikesearchQuery =
+                "<input class='boxsizing' type='text' placeholder='enter your name...' name='login' value='$login' $disabled >";
+                echo $bikesearchQuery;
+                ?>
+                <button name='quiz-logout' class="boxsizing lightbluecolor" type="submit" value='logout'
+                <?php echo empty($_SESSION['login']) ? 'disabled="true" style="background-color: #C0C0C0;pointer-events: none;"' : ''; ?>
+                >logout</button>
+                <button name='quiz-login' class="boxsizing" type="submit" value='submit'
+                <?php echo empty($_SESSION['login']) ? '' : 'disabled="true" style="background-color: #C0C0C0;pointer-events: none;"'; ?>
+                >login</button>
+                <p class='required-text' style='padding-top: 1em;padding-right: 9em;'>
+                  <?php
+                    //for detecting empty value (search button clicked but query is empty)
+                    if (isset($_POST['quiz-login']) && empty($_POST['login'])) {
+                        $_SESSION["login"] = '';
+                        session_unset();
+                        echo '*login cannot be blank'; 
+                    }
+                    elseif(isset($_POST['quiz-login']) && !empty($_POST['login'])) {
+                        //$bikeList = $GLOBALS['BikesforSale'];
+                        $validateLogin = $_POST['login'];
+                        $loginStatus = true;
+                        //search for name
+                        // foreach ($bikeList as $key => $instance) {
+                        //     $name = $instance->name;
+                        //     if($validateLogin === $name) {
+                        //       $loginStatus = true;
+                        //         break;
+                        //     }
+                        // }
 
-            <h2 class="centerText">Welcome to Takoko ! Your one stop shop for all your bike needs.</h2>
+                        //if login status is successful
+                        if($loginStatus) { 
+                           $_SESSION["login"] = $validateLogin;
+                           //refresh UI to update session
+                           header("Refresh: 0.1");
+                        }
+                        else {
+                           $_SESSION["login"] = '';
+                           session_unset();
+                           echo '*name does not exist or not in system';
+                        }
+                    }
+                    elseif(isset($_POST['quiz-logout'])) {
+                        session_destroy();
+                        header("Refresh: 0.1");
+                    }
+                  ?>
+                </p>
+              </form>
+              <!-- login using name -->
+            </section>
 
-            <div style="text-align:center; padding-bottom: 2em;">
-              <a href="bikelisting.php"><button class="bgprimarycolor" style="height: 5em; width: 20em;">Visit Listing Today!</button></a>
-            </div>
+            <section id="quizselection-section" style="<?php echo(empty($_SESSION['login']) ? 'display: none;' : '') ?>">
+              <h2 class="centerText">
+                Select the quiz you would like to take:
+              </h2>
+
+              <div class="buttons-homepage" style="padding-bottom: 2em;text-align: center;">
+                <div class="action-btn-homepage">
+
+                  <button
+                    name="submit"
+                    style="height: 5em;width: 20em;"
+                    class="action-btn-homepage buttons-homepage-btnsize bgprimarycolor"
+                    type="submit"
+                    value="Save"
+                  >
+                    Math
+                  </button>
+
+                  <button 
+                   name="submit"
+                   style="height: 5em;width: 20em;"
+                   class="action-btn-homepage buttons-homepage-btnsize bgprimarycolor"
+                   type="submit"
+                   value="Cancel" 
+                   >
+                    Literature
+                   </button>
+
+                  <p id="saved"></p>
+
+                </div>
+               </div>
+             </section>
+
           </div>
-
         </div>
 
         <div id="footer">
